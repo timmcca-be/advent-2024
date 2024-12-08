aa081e6fd30883928d169865a51ea5cdc1d3dba5
Tim McCabe <me@timmcca.be>
2024-12-08 12:51:00 -0500

    Solve day 4 part 2

--- day_4/main.py
+++ day_4/main.py
@@ -38,5 +38,27 @@ def solve_part_1(input_str):
                     count += 1
     return count
 
+def is_m_and_s(a, b):
+    return (a == "M" and b == "S") or (a == "S" and b == "M")
+
+def solve_part_2(input_str):
+    lines = input_str.splitlines()
+    count = 0
+    for line_index in range(1, len(lines) - 1):
+        line = lines[line_index]
+        for char_index in range(len(line) - 1):
+            center = line[char_index]
+            top_left = lines[line_index - 1][char_index - 1]
+            top_right = lines[line_index - 1][char_index + 1]
+            bottom_left = lines[line_index + 1][char_index - 1]
+            bottom_right = lines[line_index + 1][char_index + 1]
+            if (center == "A"
+                and is_m_and_s(top_left, bottom_right)
+                and is_m_and_s(top_right, bottom_left)):
+                count += 1
+    return count
+
 print("Part 1 example:", solve_part_1(example_input))
 print("Part 1 real:   ", solve_part_1(puzzle_input))
+print("Part 2 example:", solve_part_2(example_input))
+print("Part 2 real:   ", solve_part_2(puzzle_input))
