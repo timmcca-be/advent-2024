18b315630342a6d8c0649a430016eeafdc4e5378
Tim McCabe <timmccabe@khanacademy.org>
2024-12-20 15:14:38 -0500

    Solve day 20 part 2

--- day_20/main.py
+++ day_20/main.py
@@ -21,6 +21,9 @@ def move(position, vector):
         position[1] + vector[1]
     )
 
+def get_absolute_distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
 def parse_input(input_lines):
     width = len(input_lines[0])
     height = len(input_lines)
@@ -36,74 +39,45 @@ def parse_input(input_lines):
                 end = position
     return start, end, walls, (width, height)
 
-def get_distance_without_cheating(start, end, walls):
-    visited = set()
-    positions = {start}
-    distance = 0
-    while end not in positions:
-        visited |= positions
-        new_positions = set()
-        for position in positions:
-            for vector in vectors:
-                new_position = move(position, vector)
-                if new_position not in walls | visited:
-                    new_positions.add(new_position)
-        positions = new_positions
-        distance += 1
-    return distance
-
-def solve_part_1(input_lines):
+def solve(input_lines, max_cheat_distance):
     start, end, walls, dimensions = parse_input(input_lines)
-    cheats = set()
-    cheat_distances = dict()
-    cheats_and_distances = []
-    visited = set()
+    distances = {start: 0}
+    positions_and_distances = [(0, start)]
     positions = {start}
     distance = 0
     while end not in positions:
-        visited |= positions
         new_positions = set()
+        distance += 1
         for position in positions:
             for vector in vectors:
                 new_position = move(position, vector)
-                if new_position in visited:
+                if new_position in distances or new_position in walls:
                     continue
-                if new_position not in walls:
-                    new_positions.add(new_position)
-                    continue
-                cheat_position = move(new_position, vector)
-                cheat = (position, new_position)
-                cheat_distance = distance + 2
-                if (not is_in_bounds(dimensions, cheat_position)
-                    or cheat_position in walls
-                    or cheat in cheats):
-                    continue
-                cheats.add(cheat)
-                cheats_and_distances.append(
-                    (cheat_distance, cheat_position))
-                if cheat_position in cheat_distances:
-                    cheat_distances[cheat_position].append(cheat_distance)
-                else:
-                    cheat_distances[cheat_position] = [cheat_distance]
+                new_positions.add(new_position)
+                distances[new_position] = distance
+                positions_and_distances.append((distance, new_position))
         positions = new_positions
-        distance += 1
     target_distance = distance - 100
-    visited = set()
+    visited = {end}
     positions = {end}
     distance = 0
     result = 0
-    while len(cheats_and_distances) > 0:
-        min_cheat_distance, _ = cheats_and_distances[0]
-        if min_cheat_distance + distance > target_distance:
+    while True:
+        min_distance_from_start, _ = positions_and_distances[0]
+        if min_distance_from_start + distance > target_distance:
             break
         visited |= positions
         new_positions = set()
         for position in positions:
-            if position in cheat_distances:
-                for cheat_distance in cheat_distances[position]:
-                    if cheat_distance + distance <= target_distance:
-                        result += 1
-                    cheats_and_distances.remove((cheat_distance, position))
+            for distance_from_start, start_position in positions_and_distances:
+                if distance_from_start + distance > target_distance:
+                    break
+                distance_apart = get_absolute_distance(
+                    start_position, position)
+                if (distance_apart <= max_cheat_distance
+                    and distance_from_start + distance + distance_apart
+                    <= target_distance):
+                    result += 1
             for vector in vectors:
                 new_position = move(position, vector)
                 if new_position not in visited | walls:
@@ -111,6 +85,12 @@ def solve_part_1(input_lines):
         positions = new_positions
         distance += 1
     return result
+
+def solve_part_1(input_lines):
+    return solve(input_lines, 2)
+
+def solve_part_2(input_lines):
+    return solve(input_lines, 20)
 
 fake_newline = "$"
 eof_indicator = "<<eof>>"
@@ -128,3 +108,4 @@ while True:
 input_lines = [line.strip() for line in input_str.split(fake_newline)]
 
 print("Part 1:", solve_part_1(input_lines))
+print("Part 2:", solve_part_2(input_lines))
